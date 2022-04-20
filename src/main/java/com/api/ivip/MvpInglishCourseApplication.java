a9e44c155e05d19bc4b7220b769d0bb72438680e
cuadraO <omar.cuadrado@umgconsult.com>
2022-04-19 20:55:30 -0500

    initial commit

---
+++ src/main/java/com/api/ivip/MvpInglishCourseApplication.java
@@ -0,0 +1,13 @@
+package com.api.ivip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpInglishCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpInglishCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ivip/MvpInglishCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ivip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpInglishCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
